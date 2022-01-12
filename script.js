ce0b1bcecdcb10f79e6e52b750949cf6a81e8814
Konashkov <79665788+Konashkov@users.noreply.github.com>
2022-01-12 18:07:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let a = Number(prompt("Input your first number"))
+console.log(a)
+let b = Number(prompt("Input your second number"))
+console.log(b)
+if(a>b){
+    alert(a)
+}
+else if(b>a){
+    alert(b)
+}
+else alert("Numbers are equal")
